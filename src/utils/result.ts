2eafd394c2ae770cfc8ac50fd5c9de04d4a2a4fd
Ivan Brajkovic <ivan.brajkovic@icloud.com>
2024-09-10 20:42:55 +0000

    Refactor successResult and errorResult functions

--- src/utils/result.ts
+++ src/utils/result.ts
@@ -2,12 +2,14 @@ export type Result<TError, TData> =
   | { success: true; data: TData }
   | { success: false; error: TError };
 
-export const successResult = <T>(data: T): Result<never, T> => ({
-  success: true,
-  data,
-});
+export const successResult = <T>(data: T): Result<never, T> => {
+  return { success: true, data };
+};
 
-export const errorResult = <T>(error: T): Result<T, never> => ({
-  success: false,
-  error,
-});
+export const errorResult = <T>(error: T): Result<T, never> => {
+  // Log error in development and server environments
+  if (typeof window === 'undefined') console.error(error);
+  else if (process.env.NODE_ENV === 'development') console.error(error);
+
+  return { success: false, error };
+};
