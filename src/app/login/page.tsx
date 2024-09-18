dff239cfcd6234a598d8c56ebfd5cf7ce1c054da
Ivan Brajkovic <ivan.brajkovic@icloud.com>
2024-09-18 17:32:59 +0000

    fix: Update height utility class in Login component for better responsiveness

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/features/auth/login/login-form';
 
 export default function Login() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-svh items-center justify-center">
       <LoginForm />
     </div>
   );
